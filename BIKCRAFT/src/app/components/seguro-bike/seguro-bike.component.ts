849450a76eed28fdd71d746155ced436d77cdb79
Robert <robertrdossantos317@gmail.com>
2024-08-07 20:30:01 -0300

    estrutura dos site montada

---
+++ BIKCRAFT/src/app/components/seguro-bike/seguro-bike.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seguro-bike',
+  templateUrl: './seguro-bike.component.html',
+  styleUrls: ['./seguro-bike.component.css']
+})
+export class SeguroBikeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
